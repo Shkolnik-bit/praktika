7530d9d80d818a7a19b86a094d33e26e3b78b5ca
Эдуард <154371138+eduarduryta@users.noreply.github.com>
2026-03-04 17:42:57 +0200

    Add files via upload
    Firebase для подключения к базе данных

---
+++ services/firebaseService.js
@@ -0,0 +1,26 @@
+import { getAuth, signInWithEmailAndPassword, signOut } from 'firebase/auth'
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyD_mvSHO3Mt_lk4iM8gIMCoqfK63-3Qvis',
+	authDomain: 'praktikask-caa0b.firebaseapp.com',
+	projectId: 'praktikask-caa0b',
+	storageBucket: 'praktikask-caa0b.firebasestorage.app',
+	messagingSenderId: '638525976207',
+	appId: '1:638525976207:web:865c01a5b7981e0d7a8afa',
+	measurementId: 'G-L2B8MK8RTV',
+}
+
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+
+//  Вход
+export async function login(email, password) {
+	return await signInWithEmailAndPassword(auth, email, password)
+}
+
+//  Выход
+export async function logout() {
+	return await signOut(auth)
+}
+
+export { auth }
